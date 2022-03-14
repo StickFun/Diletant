ee9d6581d9247c6111e91efce2493e86a415066f
Sergey <49068814+StickFun@users.noreply.github.com>
2022-03-14 08:36:20 +0400

    Fixed name_form

--- FirefoxClient.py
+++ FirefoxClient.py
@@ -13,13 +13,16 @@ def getlastmessage(driver):
 
 def clickrequired(driver, name, namebox):
     name_form = driver.find_element(By.ID,
-                                    namebox)  # Нужно ID ссылки конференции засовывать в строку
+                                    namebox)
     name_form.send_keys(name)
-
-    button_join = driver.find_element(By.ID, 'room-join')
+    print('имя введено')
+    button_join = driver.find_element(By.CSS_SELECTOR, '#room-join')
     button_join.click()
 
-    time.sleep(5)
+    button_presence = driver.find_elements(By.CSS_SELECTOR, "[aria-label='Только слушать']")
+    while len(button_presence) < 1:
+        time.sleep(5)
+        button_presence = driver.find_elements(By.CSS_SELECTOR, "[aria-label='Только слушать']")
 
     button_listen = driver.find_element(By.CSS_SELECTOR, "[aria-label='Только слушать']")
     button_listen.click()
@@ -33,15 +36,19 @@ def join(link, name):
         clickrequired(driver, name, getnamebox(link))
     except:
          pass
-    LastMessage = getlastmessage(driver)
-    while LastMessage.lower() not in ['до свидания', 'всего доброго']:
-        time.sleep(3)
-        LastMessage = getlastmessage(driver)
+
+    time.sleep(5)
+
+    lastmessage = getlastmessage(driver)
+    while lastmessage.lower() not in ['до свидания', 'всего доброго']:
+        time.sleep(5)
+        lastmessage = getlastmessage(driver)
 
     Chat_Box = driver.find_element(By.CSS_SELECTOR, '#message-input')
     Chat_Box.send_keys(Additional.saygoodbye())
 
-    Send_Button = driver.find_element(By.XPATH, '/html/body/div[1]/main/section/div[3]/section/div/form/div[1]/button/span[1]')
+    Send_Button = driver.find_element(By.XPATH,
+                                      '/html/body/div[1]/main/section/div[3]/section/div/form/div[1]/button/span[1]')
     Send_Button.click()
 
     teardown(driver)
@@ -51,5 +58,13 @@ def teardown(driver):
     driver.close()
 
 
-join('https://bbb.ssau.ru/b/gew-x5n-wxi-tcd', 'Маркелов')
+
+
+join('https://bbb.ssau.ru/b/aph-ecx-6qn','Фомин Алексей')
+
+print('succesfully present during lesson')
+
+while time.localtime().tm_hour != 11 or time.localtime().tm_min < 25:
+    pass
+join('https://bbb.ssau.ru/b/99p-hvx-egj-903', '6203 Сычев Сергей')
 
